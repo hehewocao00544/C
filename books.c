d4c8b5e9087a0aee13554e6326ef2595e86a385d
hehewocao00544 <2495399053@qq.com>
2019-01-24 22:20:11 +0800

    基本完成程序功能

--- books.c
+++ books.c
@@ -22,10 +22,16 @@ void Next();//输入y继续，输入其他键退出
 void output(struct BOOK* head);//输出链表
 void CL();//创建保存链表
 void Save();//保存提示框
+void Del();//删除提示框
 void FileSave();//文件保存
 void FileRead(struct BOOK* HEAD);//文件读取
 void DelRe(struct BOOK* HEAD);//删除重复
 void DelBook(struct BOOK* HEAD);//删除图书（编号或书名）
+void PX_b(struct BOOK* HEAD);//编号排序
+void PX_p(struct BOOK* HEAD);//价格排序
+//void PX_n(struct BOOK* HEAD);//书名排序
+void Check();//图书查询
+void Change();//图书信息修改
 //主函数：
 
 
@@ -34,25 +40,37 @@ int main(void)
 {
 	head = (struct BOOK*)malloc(sizeof(struct BOOK));
 	head->next = NULL;
-	/*JM();
+	loop:
+	JM();
 	if (ch == '1')
 	{
 		CL();
 	}
 	else if (ch == '2')
 	{
-		FileRead(head);
-		DelRe(head);
+		Check();
+	}
+	else if (ch == '3')
+	{
+		
+	}
+	else if (ch == '4')
+	{
+		Change();
+	}
+	else if (ch == '5')
+	{
+		output;
 	}
 	else if (ch == '6')
 	{
 		return 0;
-	}*/
-	FileRead(head);
-	DelBook(head);
-	output(head);
+	}
+	else
+	{
+		goto loop;
+	}
 	return 0;
-
 }
 
 
@@ -75,7 +93,7 @@ void JM()
 	gotoxy(80, 19); printf("     |  4.图书信息修改  |");
 	gotoxy(80, 21); printf("     |  5.图书信息总览  |");
 	gotoxy(80, 23); printf("     |  6.退出程序     |");
-	gotoxy(86, 25); printf("输入序号，选择功能");
+	gotoxy(86, 25); printf("      输入序号，选择功能");
 	ch = getch();
 	system("cls");
 }
@@ -106,6 +124,18 @@ void Save()
 	gotoxy(80, 26); printf("|          y / n          |\n");
 	gotoxy(80, 28); printf("|                         |\n");
 	gotoxy(80, 30); printf("---------------------------\n");
+	gotoxy(80, 32);	printf("     输入y保存！输入n取消！");
+}
+
+void Del()
+{
+	system("cls");
+	gotoxy(80, 20); printf("---------------------------\n");
+	gotoxy(80, 22); printf("|                         |\n");
+	gotoxy(80, 24); printf("|    Are you delete it ?    |\n");
+	gotoxy(80, 26); printf("|          y / n          |\n");
+	gotoxy(80, 28); printf("|                         |\n");
+	gotoxy(80, 30); printf("---------------------------\n");
 	gotoxy(80, 32);	printf("        输入y保存！");
 }
 
@@ -115,11 +145,18 @@ void DelJM()
 	gotoxy(80, 11); printf("*********图书管理系统*********");
 	gotoxy(80, 13); printf("     |  1.删除其重复信息  |");
 	gotoxy(80, 15); printf("     |  2.按图书编号删除  |");
-	gotoxy(80, 17); printf("     |  3.按图书编号删除  |");
-	gotoxy(80, 19); printf("     |  4.按图书编号删除  |");
-	gotoxy(80, 21); printf("     |  5.按图书编号删除  |");
-	gotoxy(80, 23); printf("     |  6.返回系统主界面  |");
+	gotoxy(80, 17); printf("     |  3.返回系统主界面  |");
 	gotoxy(86, 25); printf(" 输入序号，选择功能");
+}
+
+void PXJM()
+{
+	system("cls");
+	gotoxy(80, 11); printf("*********图书管理系统*********");
+	gotoxy(80, 13); printf("     |  1.按图书编号排序  |");
+	gotoxy(80, 15); printf("	 |  2.按图书价格排序  |");
+	gotoxy(80, 17); printf("     |  3.返回系统主界面  |");
+	gotoxy(86, 29); printf(" 输入序号，选择功能");
 }
 
 
@@ -240,38 +277,46 @@ void FileRead(struct BOOK* HEAD)
 
 void DelRe(struct BOOK* HEAD)
 {
+	int a = 1;//删除后链表长度计数器
 	struct BOOK* q = (struct BOOK*)malloc(sizeof(struct BOOK));
+	q->next = NULL;
 	struct BOOK* q1 = (struct BOOK*)malloc(sizeof(struct BOOK));
+	q1->next = NULL;
 	struct BOOK* q2 = (struct BOOK*)malloc(sizeof(struct BOOK));
+	q2->next = NULL;
 	if (HEAD->next != NULL)
 	{
 		q = HEAD->next;
-		while (q->next != NULL)
+	}
+	while (q->next->next!= NULL)
+	{
+		a++;
+		q1 = q;
+		q2 = q1;
+		while (q1->next!=NULL)
 		{
-			q1 = q;
-			q2 = q->next;
-			while (q2->next != NULL)
+			q2 = q1->next;
+			if (q2->next != NULL&&strcmp(q2->bianhao, q->bianhao) == 0)
 			{
-				if (strcmp(q->bianhao,q2->bianhao)==0)
-				{
-					q1->next = q2->next;
-					continue;
-				}
-				else
-				{
-					q1 = q1->next;
-				}
+				q1->next = q2->next;
 				q2 = q2->next;
+				continue;
 			}
-			if (strcmp(q->bianhao,q2->bianhao)==0)
+			else if (q2->next == NULL&&strcmp(q2->bianhao, q->bianhao) == 0)
 			{
 				q1->next = NULL;
+				break;
 			}
-			q = q->next;
+			q1 = q1->next;
 		}
+		q = q->next;
 	}
-	output(head);
-	/*FILE* fp = fopen("bookinformation.txt", "wt");
+	if (strcmp(q2->bianhao, q->bianhao) == 0)
+	{
+		q->next = NULL;
+	}
+	q = HEAD->next;
+	FILE* fp = fopen("bookinformation.txt", "wt");
 	if (fp == NULL)
 	{
 		printf("打开文件失败！\n");
@@ -283,11 +328,12 @@ void DelRe(struct BOOK* HEAD)
 		q = q->next;
 	}
 	fclose(fp);
-	*/
+	
 }
 
 void DelBook(struct BOOK* HEAD)
 {
+	int a = 0;//删除后链表长度计数器
 	char ch[50];
 	struct BOOK* q1 = (struct BOOK*)malloc(sizeof(struct BOOK));
 	q1->next = NULL;
@@ -298,18 +344,322 @@ void DelBook(struct BOOK* HEAD)
 	scanf("%s", ch);
 	while (q1->next != NULL)
 	{
+		a++;
 		q2 = q1->next;
 		if (q2->next!=NULL && (strcmp(ch, q2->bianhao) == 0 || strcmp(ch, q2->bookname) == 0))
 		{
+			a--;
 			q1->next = q2->next;
 			q2 = q2->next;
 			continue;
 		}
 		else if (q2->next == NULL && (strcmp(ch, q2->bianhao) == 0 || strcmp(ch, q2->bookname) == 0))
 		{
+			a--;
 			q1->next = NULL;
 			break;
 		}
 		q1 = q1->next;
+	}
+	q1 = HEAD->next;
+	FILE* fp = fopen("bookinformation.txt", "wt");
+	if (fp == NULL)
+	{
+		printf("打开文件失败！\n");
+		return 0;
+	}
+	for (int i = 0; i < a; i++)
+	{
+		fprintf(fp, "%s %s %s %s %s %s %g\n", q1->bianhao, q1->bookname, q1->writer, q1->leibie, q1->chubanfang, q1->data, q1->price);
+		q1 = q1->next;
+	}
+	fclose(fp);
+}
+
+void PX_b(struct BOOK* HEAD)
+{
+	struct BOOK* q = (struct BOOK*)malloc(sizeof(struct BOOK));
+	q = HEAD;
+	int a = 0;//链表长度计数器
+	while (q->next != NULL)
+	{
+		a++;
+		q = q->next;
+	}
+	struct BOOK* q1 = (struct BOOK*)malloc(sizeof(struct BOOK));
+	q1->next = NULL;
+	struct BOOK* q2 = (struct BOOK*)malloc(sizeof(struct BOOK));
+	q2->next = NULL;
+	if (HEAD->next->next != NULL)
+	{
+		for (int i=0;i<a-1;i++)
+		{
+			q = HEAD;
+			q1 = q->next;
+			q2 = q1->next;
+			while (q2->next != NULL)
+			{
+				if (strcmp(q1->bianhao, q2->bianhao) > 0)
+				{
+					q->next = q1->next;
+					q1->next = q2->next;
+					q2->next = q1;
+					q2 = q1->next;
+					q = q->next;
+					continue;
+				}
+				q = q->next;
+				q1 = q1->next;
+				q2 = q2->next;
+			}
+			if (strcmp(q1->bianhao, q2->bianhao) > 0)
+			{
+				q->next = q2;
+				q2->next = q1;
+				q1->next = NULL;
+			}
+		}
+	}
+	q1 = HEAD->next;
+	FILE* fp = fopen("bookinformation.txt", "wt");
+	if (fp == NULL)
+	{
+		printf("打开文件失败！\n");
+		return 0;
+	}
+	for (int i = 0; i < a; i++)
+	{
+		fprintf(fp, "%s %s %s %s %s %s %g\n", q1->bianhao, q1->bookname, q1->writer, q1->leibie, q1->chubanfang, q1->data, q1->price);
+		q1 = q1->next;
+	}
+	fclose(fp);
+}
+
+void PX_p(struct BOOK* HEAD)
+{
+	struct BOOK* q = (struct BOOK*)malloc(sizeof(struct BOOK));
+	q = HEAD;
+	int a = 0;//链表长度计数器
+	while (q->next != NULL)
+	{
+		a++;
+		q = q->next;
+	}
+	struct BOOK* q1 = (struct BOOK*)malloc(sizeof(struct BOOK));
+	q1->next = NULL;
+	struct BOOK* q2 = (struct BOOK*)malloc(sizeof(struct BOOK));
+	q2->next = NULL;
+	if (HEAD->next->next != NULL)
+	{
+		for (int i = 0; i< a - 1; i++)
+		{
+			q = HEAD;
+			q1 = q->next;
+			q2 = q1->next;
+			while (q2->next != NULL)
+			{
+				if (q1->price > q2->price)
+				{
+					q->next = q1->next;
+					q1->next = q2->next;
+					q2->next = q1;
+					q2 = q1->next;
+					q = q->next;
+					continue;
+				}
+				q = q->next;
+				q1 = q1->next;
+				q2 = q2->next;
+			}
+			if (q1->price > q2->price)
+			{
+				q->next = q2;
+				q2->next = q1;
+				q1->next = NULL;
+			}
+		}
+	}
+	q1 = HEAD->next;
+	FILE* fp = fopen("bookinformation.txt", "wt");
+	if (fp == NULL)
+	{
+		printf("打开文件失败！\n");
+		return 0;
+	}
+	for (int i = 0; i < a; i++)
+	{
+		fprintf(fp, "%s %s %s %s %s %s %g\n", q1->bianhao, q1->bookname, q1->writer, q1->leibie, q1->chubanfang, q1->data, q1->price);
+		q1 = q1->next;
+	}
+	fclose(fp);
+}
+
+/*void PX_n(struct BOOK* HEAD)
+{
+	struct BOOK* q = (struct BOOK*)malloc(sizeof(struct BOOK));
+	q = HEAD;
+	int a = 0;//链表长度计数器
+	while (q->next != NULL)
+	{
+		a++;
+		q = q->next;
+	}
+	struct BOOK* q1 = (struct BOOK*)malloc(sizeof(struct BOOK));
+	q1->next = NULL;
+	struct BOOK* q2 = (struct BOOK*)malloc(sizeof(struct BOOK));
+	q2->next = NULL;
+	if (HEAD->next->next != NULL)
+	{
+		for (int i = 0; i<a - 1; i++)
+		{
+			q = HEAD;
+			q1 = q->next;
+			q2 = q1->next;
+			while (q2->next != NULL)
+			{
+				if (strcmp(q1->bookname, q2->bookname) > 0)
+				{
+					q->next = q1->next;
+					q1->next = q2->next;
+					q2->next = q1;
+					q2 = q1->next;
+					q = q->next;
+					continue;
+				}
+				q = q->next;
+				q1 = q1->next;
+				q2 = q2->next;
+			}
+			if (strcmp(q1->bookname, q2->bookname) > 0)
+			{
+				q->next = q2;
+				q2->next = q1;
+				q1->next = NULL;
+			}
+		}
+	}
+	q1 = HEAD->next;
+	FILE* fp = fopen("bookinformation.txt", "wt");
+	if (fp == NULL)
+	{
+		printf("打开文件失败！\n");
+		return 0;
+	}
+	for (int i = 0; i < a; i++)
+	{
+		fprintf(fp, "%s %s %s %s %s %s %g\n", q1->bianhao, q1->bookname, q1->writer, q1->leibie, q1->chubanfang, q1->data, q1->price);
+		q1 = q1->next;
+	}
+	fclose(fp);
+}*/
+
+void Change()
+{
+	system("cls");
+	FileRead(head);
+	struct BOOK* q = (struct BOOK*)malloc(sizeof(struct BOOK));
+	q = head;
+	char ch[50];
+	gotoxy(80, 15); printf("请输入需要查询图书的编号或书名：");
+	gotoxy(80, 17); scanf("%s", ch);
+	while (q->next != NULL)
+	{
+		if (strcmp(ch, q->next->bianhao) == 0 || strcmp(ch, q->next->bookname) == 0)
+		{
+			struct BOOK* q1 = (struct BOOK*)malloc(sizeof(struct BOOK));
+			q1 = q->next;
+			system("cls");
+			struct BOOK* s = (struct BOOK*)malloc(sizeof(struct BOOK));
+			gotoxy(80, 11); printf("请重新输入图书编号（小于10位）：");
+			scanf("%s", s->bianhao);
+			gotoxy(80, 13); printf("请重新输入书名（小于25字）：");
+			scanf("%s", s->bookname);
+			gotoxy(80, 15); printf("请重新输入作者姓名（小于25字）：");
+			scanf("%s", s->writer);
+			gotoxy(80, 17); printf("请重新输入图书类别（小于10字）：");
+			scanf("%s", s->leibie);
+			gotoxy(80, 19); printf("请重新输入图书出版方（小于25字）：");
+			scanf("%s", s->chubanfang);
+			gotoxy(80, 21); printf("请重新输入图书出版日期（如2018.03.03）：");
+			scanf("%s", s->data);
+			gotoxy(80, 23); printf("请重新输入图书价格：");
+			scanf("%f", &s->price);
+			char a;
+			Save();
+			loop:a = getch();
+			if (a == 'y' || a == 'Y')
+			{
+				q->next = s;
+				s->next = q1->next;
+			}
+			else if (a == 'n' || a == 'N')
+			{
+				system("cls");
+				gotoxy(90, 25); printf("修改未保存！");
+				Sleep(1000);
+			}
+			else
+			{
+				gotoxy(80, 34); printf("请重新输入y或n！");
+				goto loop;
+			}
+		}
+		q = q->next;
+	}
+	q = head;
+	int a = 0;//链表长度计数器
+	while (q->next != NULL)
+	{
+		a++;
+		q = q->next;
+	}
+	q = head->next;
+	FILE* fp = fopen("bookinformation.txt", "wt");
+	if (fp == NULL)
+	{
+		printf("打开文件失败！\n");
+		return 0;
+	}
+	for (int i = 0; i < a; i++)
+	{
+		fprintf(fp, "%s %s %s %s %s %s %g\n", q->bianhao, q->bookname, q->writer, q->leibie, q->chubanfang, q->data, q->price);
+		q = q->next;
+	}
+	fclose(fp);
+}
+
+void Check()
+{
+	system("cls");
+	FileRead(head);
+	struct BOOK* q = (struct BOOK*)malloc(sizeof(struct BOOK));
+	q = head;
+	char ch[50];
+	gotoxy(80, 15); printf("请输入需要查询图书的编号或书名：");
+	gotoxy(80, 17); scanf("%s", ch);
+	int t = 0;
+	while (q->next != NULL)
+	{
+		if (strcmp(ch, q->next->bianhao) == 0 || strcmp(ch, q->next->bookname) == 0)
+		{
+			t++;
+			system("cls");
+			gotoxy(33, 6); printf("-----------------------------------------------------------------------------------------------------------------\n");
+			gotoxy(40, 7); printf("图书编号        书名         作者         图书类别         出版方         出版日期         图书价格\n");
+			gotoxy(33, 8); printf("-----------------------------------------------------------------------------------------------------------------\n");
+			gotoxy(40, 9); printf("%s", q->next->bianhao);
+			gotoxy(55, 9); printf("%s", q->next->bookname);
+			gotoxy(69, 9); printf("%s", q->next->writer);
+			gotoxy(82, 9); printf("%s", q->next->leibie);
+			gotoxy(99, 9); printf("%s", q->next->chubanfang);
+			gotoxy(114, 9); printf("%s", q->next->data);
+			gotoxy(131, 9); printf("%g\n", q->next->price);
+		}
+		q = q->next;
+	}
+	if (t == 0)
+	{
+		system("cls");
+		gotoxy(80, 25); printf("这本书还没有添加入库！！！");
 	}
 }
